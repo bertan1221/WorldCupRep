8baca1f0ae428793ad41f8ebf148390bc1c6a67d
bertan1221 <b3rtan@live.com>
2018-06-09 19:01:28 +0200

    update 2.0

--- WorldCup.Data/WorldCupDbInitializer .cs
+++ WorldCup.Data/WorldCupDbInitializer .cs
@@ -12,52 +12,52 @@ namespace WorldCup.Data
     {
         protected override void Seed(WorldCupDbContext context)
         {
-            var germany = new Team
-            {
-                TeamName = "Germany",
-                TeamContinent = "Europe",
-                TeamWCwins = 4,
-                TeamWCappearances = 20,
+            //var germany = new Team
+            //{
+            //    TeamName = "Germany",
+            //    TeamContinent = "Europe",
+            //    TeamWCwins = 4,
+            //    TeamWCappearances = 20,
                 
                 
-            };
+            //};
 
-            var argentina = new Team
-            {
-                TeamName = "Argentina",
-                TeamContinent = "South America",
-                TeamWCwins = 0,
-                TeamWCappearances = 20,
+            //var argentina = new Team
+            //{
+            //    TeamName = "Argentina",
+            //    TeamContinent = "South America",
+            //    TeamWCwins = 0,
+            //    TeamWCappearances = 20,
                 
                 
-            };
+            //};
 
-            context.Teams.Add(germany);
-            context.Teams.Add(argentina);
+            //context.Teams.Add(germany);
+            //context.Teams.Add(argentina);
 
-            context.Players.Add(new Player
-            {
-                PlayerAge = 32,
-                PlayerJerseyNumber = 14,
-                PlayerName = "Manuel Neuer",
-                PlayerPosition = "GK",
-                PlayerWCappearances = 4,
-                PlayerWCgoalsScored = 1,
-                Team = germany
+            //context.Players.Add(new Player
+            //{
+            //    PlayerAge = 32,
+            //    PlayerJerseyNumber = 14,
+            //    PlayerName = "Manuel Neuer",
+            //    PlayerPosition = "GK",
+            //    PlayerWCappearances = 4,
+            //    PlayerWCgoalsScored = 1,
+            //    Team = germany
 
-            });
+            //});
 
-            context.Matches.Add(new Match
-            {
+            //context.Matches.Add(new Match
+            //{
                 
-                MatchAwayTeam = germany,
-                MatchHomeTeam = argentina,
-                MatchAwayTeamName = germany.TeamName,
-                MatchHomeTeamName = argentina.TeamName,
-                MatchHomeTeamId = 20,
-                MatchAwayTeamId = 10,
-                MatchKickOff = new DateTime(2018, 07, 03)
-            });
+            //    MatchAwayTeam = germany,
+            //    MatchHomeTeam = argentina,
+            //    MatchAwayTeamName = germany.TeamName,
+            //    MatchHomeTeamName = argentina.TeamName,
+            //    MatchHomeTeamId = 20,
+            //    MatchAwayTeamId = 10,
+            //    MatchKickOff = new DateTime(2018, 07, 03)
+            //});
 
             base.Seed(context);
         }
